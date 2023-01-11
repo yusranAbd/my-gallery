efec404c9f7a85f3069bcc1c9bc999c4966483f4
yusranAbd <yusran.abd.arief@gmail.com>
2023-01-11 22:30:21 +0700

    fetching data from supabase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,62 @@
+import { createClient } from "@supabase/supabase-js"
+import Image from "next/image"
+import { useState } from "react"
 
-export default function Gallery() {
+export async function getStaticProps() {
+  const supabaseAdmin = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL || '',
+    process.env.SUPABASE_SERVICE_ROLE_KEY || '',
+  )
+
+  const {data} = await supabaseAdmin.from('images').select('*').order('id')
+  return {
+    props: {images: data}
+  }
+}
+
+function cn(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
+type Image = {
+  id: number
+  href: string
+  imageSrc: string
+  name: string
+  username: string
+}
+
+export default function Gallery({images}: {images:Image[]}) {
   return (
     <div className="max-w2xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
       <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-    <Image/>
+        {images.map((image) =>(
+          <BlurImage key={image.id} image={image}/>
+        ))}
       </div>
     </div>
   )
 }
 
-function Image() {
+function BlurImage({image}: {image: Image}) {
+  const [isLoading, setLoading] = useState(true)
+
+
   return(
     <a className="group" href="#">
         <div className="aspect-w-1 aspect-h-1 xl:aspect-w-7 xl:aspect-h-8 w-full overflow-hidden rounded-lg bg-gray-200">
-          <img src="https://bit.ly/placeholder-img" alt="" className="group-hover:opacity-75"/>
+          <Image 
+          src={image.imageSrc}
+          alt=""  
+          layout="fill" 
+          objectFit="cover" 
+          className={cn(
+            "group-hover:opacity-75 duration-700 ease-in-out", 
+            isLoading 
+            ? 'grayscale blur-2xl scale-110' 
+            : 'grayscale-0 blur-0 scale-100')} 
+            onLoadingComplete={() => setLoading(false)}
+            />
         </div>
         <h3 className="mt-4 text-sm text-gray-700">Yusran Abdullah</h3>
         <p className="mt-1 text-lg font-medium text-gray-900">@ydoublea</p>
